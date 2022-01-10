205cd1260cbdff875ea5a6aff8be4236cb854cdf
Storm Søndergaard <stormsoendergaard@gmail.com>
2022-01-10 01:48:17 +0100

    ImGui dockspaces

--- Kronos/Source/Kronos/main.cpp
+++ Kronos/Source/Kronos/main.cpp
@@ -6,64 +6,131 @@
 
 int main(int argc, char* argv[])
 {
-    GLFWwindow* window;
+	GLFWwindow* window;
 
-    // Initialize glfw
-    if (!glfwInit()) return -1;
+	// Initialize glfw
+	if (!glfwInit()) return -1;
 
-    // Create GLFW window
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
-    if (!window)
-    {
-        glfwTerminate();
-        return -1;
-    }
-    glfwMakeContextCurrent(window);
-    glfwSwapInterval(1); // Enable vsync
+	// Create GLFW window
+	window = glfwCreateWindow(1280, 720, "Hello World", NULL, NULL);
+	if (!window)
+	{
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+	glfwSwapInterval(1); // Enable vsync
 
-    const char* glsl_version = "#version 130";
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+	const char* glsl_version = "#version 130";
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
 
-    // Initialize  ImGui
-    IMGUI_CHECKVERSION();
-    ImGui::CreateContext();
-    ImGui::StyleColorsDark();
-    ImGui_ImplGlfw_InitForOpenGL(window, true);
-    ImGui_ImplOpenGL3_Init(glsl_version);
+	// Initialize  ImGui
+	IMGUI_CHECKVERSION();
+	ImGui::CreateContext();
+	ImGui::StyleColorsDark();
+	ImGui_ImplGlfw_InitForOpenGL(window, true);
+	ImGui_ImplOpenGL3_Init(glsl_version);
+	ImGui::GetIO().ConfigFlags |= ImGuiConfigFlags_DockingEnable;
 
-    // Initialize glad
-    gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
+	// Initialize glad
+	gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
 
-    while (!glfwWindowShouldClose(window))
-    {
-        glfwPollEvents();
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwPollEvents();
 
-        ImGui_ImplOpenGL3_NewFrame();
-        ImGui_ImplGlfw_NewFrame();
-        ImGui::NewFrame();
+		// Call ImGui NewFrame functions
+		ImGui_ImplOpenGL3_NewFrame();
+		ImGui_ImplGlfw_NewFrame();
+		ImGui::NewFrame();
 
-        ImGui::ShowDemoWindow();
+		// Submit ImGui commands
+		{
+			ImGui::ShowDemoWindow();
 
-        ImGui::Render();
-        int display_w, display_h;
-        glfwGetFramebufferSize(window, &display_w, &display_h);
-        glViewport(0, 0, display_w, display_h);
-        glClearColor(0.05f, 0.05f, 0.05f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
-        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+			// Begin dockspace
+			{
+				ImGuiDockNodeFlags dockspace_flags = ImGuiDockNodeFlags_None;
 
-        glfwSwapBuffers(window);
-    }
+				ImGuiWindowFlags window_flags = ImGuiWindowFlags_MenuBar | ImGuiWindowFlags_NoDocking;
+				window_flags |= ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove;
+				window_flags |= ImGuiWindowFlags_NoBringToFrontOnFocus | ImGuiWindowFlags_NoNavFocus;
 
-    // Terminate ImGui
-    ImGui_ImplOpenGL3_Shutdown();
-    ImGui_ImplGlfw_Shutdown();
-    ImGui::DestroyContext();
+				ImGuiViewport* viewport = ImGui::GetMainViewport();
+				ImGui::SetNextWindowPos(viewport->Pos);
+				ImGui::SetNextWindowSize(viewport->Size);
+				ImGui::SetNextWindowViewport(viewport->ID);
 
-    // Terminate GLFW
-    glfwDestroyWindow(window);
-    glfwTerminate();
+				ImGui::PushStyleVar(ImGuiStyleVar_WindowRounding, 0.0f);
+				ImGui::PushStyleVar(ImGuiStyleVar_WindowBorderSize, 0.0f);
+				ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0.0f, 0.0f));
+				ImGui::Begin("DockSpace", (bool*)0, window_flags);
+				ImGui::PopStyleVar(3);
 
-    return 0;
+				ImGui::DockSpace(ImGui::GetID("MyDockSpace"), ImVec2(0.0f, 0.0f), dockspace_flags);
+			}
+
+			// Window menu bar
+			{
+				if (ImGui::BeginMenuBar())
+				{
+					if (ImGui::BeginMenu("File"))
+					{
+						ImGui::MenuItem("New", "Ctrl+N");
+						ImGui::MenuItem("Open...", "Ctrl+O");
+						ImGui::MenuItem("Save As...", "Ctrl+Shift+S");
+						ImGui::Separator();
+						ImGui::MenuItem("Exit");
+						ImGui::EndMenu();
+					}
+
+					if (ImGui::BeginMenu("Window"))
+					{
+						ImGui::MenuItem("Editor Preferences", NULL, nullptr);
+						ImGui::EndMenu();
+					}
+
+					ImGui::EndMenuBar();
+				}
+			}
+
+			// Editor preferences
+			{
+				if (ImGui::Begin("Editor Preferences"))
+				{
+					ImGui::TextUnformatted("Test");
+				}
+			}
+
+			// End dockspace
+			{
+				ImGui::End(); // Dockspace
+			}
+
+			ImGui::End();
+		}
+
+		// Render
+		ImGui::Render();
+		int display_w, display_h;
+		glfwGetFramebufferSize(window, &display_w, &display_h);
+		glViewport(0, 0, display_w, display_h);
+		glClearColor(0.05f, 0.05f, 0.05f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+		glfwSwapBuffers(window);
+	}
+
+	// Terminate ImGui
+	ImGui_ImplOpenGL3_Shutdown();
+	ImGui_ImplGlfw_Shutdown();
+	ImGui::DestroyContext();
+
+	// Terminate GLFW
+	glfwDestroyWindow(window);
+	glfwTerminate();
+
+	return 0;
 }
