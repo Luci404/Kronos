41c5c20b9e3c68dc181c27adcd3f5a67ce576829
Storm Søndergaard <stormsoendergaard@gmail.com>
2022-01-16 17:42:55 +0100

    Scheduling infrastructure sketches

--- ArchitectureDemos/SchedulingInfrastructure_01.cpp
+++ ArchitectureDemos/SchedulingInfrastructure_01.cpp
@@ -1,28 +1,41 @@
 #include <vector>
 
-// Scheduling infrastructure
+/*
+LayerStack: A list of layers with some utility functions for layer management.
+
+Layer: A structure reposible for the execution of modules, furthermore, layers can have extra logic and data for specialized layer behavior.
+
+ModuleStack: A list of modules with some utility functions for module management.
+
+Module: Resposible for the initialization, termination and ticking of a feature or api. For some systems like the physics engine,
+the module would be responsible for initialization of PhysX at preperation for it's use in other modules on higher layers.
+Other modules might not do other than reading a config file.
+*/
+
+#pragma region SchedulingInfrastructure
+
 class Module
 {
 public:
-    virtual void Initialize() = 0;
-    virtual void Tick() = 0;
-    virtual void Terminate() = 0;
+    virtual void Initialize(){};
+    virtual void Tick(){};
+    virtual void Terminate(){};
 };
 
 class ModuleStack
 {
-public
-    Module *PushModule(Module *module) { return module; }
+public:
+    Module* PushModule(Module* module) { return module; }
 
-    std::vector<Module *>::iterator begin() { return m_Modules.begin(); }
-    std::vector<Module *>::iterator end() { return m_Modules.end(); }
-    std::vector<Module *>::reverse_iterator rbegin() { return m_Modules.rbegin(); }
-    std::vector<Module *>::reverse_iterator rend() { return m_Modules.rend(); }
+    std::vector<Module*>::iterator begin() { return m_Modules.begin(); }
+    std::vector<Module*>::iterator end() { return m_Modules.end(); }
+    std::vector<Module*>::reverse_iterator rbegin() { return m_Modules.rbegin(); }
+    std::vector<Module*>::reverse_iterator rend() { return m_Modules.rend(); }
 
-    std::vector<Module *>::const_iterator begin() const { return m_Modules.begin(); }
-    std::vector<Module *>::const_iterator end() const { return m_Modules.end(); }
-    std::vector<Module *>::const_reverse_iterator rbegin() const { return m_Modules.rbegin(); }
-    std::vector<Module *>::const_reverse_iterator rend() const { return m_Modules.rend(); }
+    std::vector<Module*>::const_iterator begin() const { return m_Modules.begin(); }
+    std::vector<Module*>::const_iterator end() const { return m_Modules.end(); }
+    std::vector<Module*>::const_reverse_iterator rbegin() const { return m_Modules.rbegin(); }
+    std::vector<Module*>::const_reverse_iterator rend() const { return m_Modules.rend(); }
 
 private:
     std::vector<Module *> m_Modules;
@@ -64,51 +77,90 @@ protected:
 class LayerStack
 {
 public:
-    Layer *PushLayer(Layer *layer) { return layer; }
+    Layer* PushLayer(Layer* layer) { return layer; }
 
-    std::vector<Layer *>::iterator begin() { return m_Layers.begin(); }
-    std::vector<Layer *>::iterator end() { return m_Layers.end(); }
-    std::vector<Layer *>::reverse_iterator rbegin() { return m_Layers.rbegin(); }
-    std::vector<Layer *>::reverse_iterator rend() { return m_Layers.rend(); }
+    std::vector<Layer*>::iterator begin() { return m_Layers.begin(); }
+    std::vector<Layer*>::iterator end() { return m_Layers.end(); }
+    std::vector<Layer*>::reverse_iterator rbegin() { return m_Layers.rbegin(); }
+    std::vector<Layer*>::reverse_iterator rend() { return m_Layers.rend(); }
 
-    std::vector<Layer *>::const_iterator begin() const { return m_Layers.begin(); }
-    std::vector<Layer *>::const_iterator end() const { return m_Layers.end(); }
-    std::vector<Layer *>::const_reverse_iterator rbegin() const { return m_Layers.rbegin(); }
-    std::vector<Layer *>::const_reverse_iterator rend() const { return m_Layers.rend(); }
+    std::vector<Layer*>::const_iterator begin() const { return m_Layers.begin(); }
+    std::vector<Layer*>::const_iterator end() const { return m_Layers.end(); }
+    std::vector<Layer*>::const_reverse_iterator rbegin() const { return m_Layers.rbegin(); }
+    std::vector<Layer*>::const_reverse_iterator rend() const { return m_Layers.rend(); }
 
 private:
-    std::vector<Layer *> m_Layers;
+    std::vector<Layer*> m_Layers;
 };
+
+#pragma endregion SchedulingInfrastructure
 
 #pragma region Layers
 
 // Platform layer
+class LoggingModule : public Module {};
+class ThreadingModule : public Module {};
+class FileIOModule : public Module {};
+
 class PlatformLayer : public Layer
 {
-    PlatformLayer() {}
+public:
+    PlatformLayer()
+    {
+        m_ModuleStack.PushModule(new LoggingModule());
+        m_ModuleStack.PushModule(new ThreadingModule());
+        m_ModuleStack.PushModule(new FileIOModule());
+    }
 };
 
 // Core layer
+class JobSystem : public Module {};
+class EngineConfig : public Module {};
+class Serializer : public Module {};
+
 class CoreLayer : public Layer
 {
-    CoreLayer() {}
+public:
+    CoreLayer()
+    {
+        m_ModuleStack.PushModule(new JobSystem());
+        m_ModuleStack.PushModule(new EngineConfig());
+        m_ModuleStack.PushModule(new Serializer());
+    }
 };
 
 // Management layer
+class SceneGraph : public Module {};
+class Networking : public Module {};
+
 class ManagementLayer : public Layer
 {
-    ManagementLayer() {}
+public:
+    ManagementLayer()
+    {
+        m_ModuleStack.PushModule(new SceneGraph());
+        m_ModuleStack.PushModule(new Networking());
+    }
 };
 
 // Simulation layer
+class Physics : public Module {};
+class EventSystem : public Module {};
+
 class SimulationLayer : public Layer
 {
-    SimulationLayer() {}
+public:
+    SimulationLayer()
+    {
+        m_ModuleStack.PushModule(new Physics());
+        m_ModuleStack.PushModule(new EventSystem());
+    }
 };
+
 #pragma endregion Layers
 
+#pragma region Application
 
-// Application (simplified)
 class Application
 {
 public:
@@ -157,3 +209,5 @@ public:
 private:
     LayerStack m_LayerStack;
 };
+
+#pragma endregion Application
