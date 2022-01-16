7d58a27478a55393951deb70e6cedf43725001be
Storm Søndergaard <stormsoendergaard@gmail.com>
2022-01-16 17:56:41 +0100

    Scheduling infrastructure sketches

--- ArchitectureDemos/SchedulingInfrastructure_02.cpp
+++ ArchitectureDemos/SchedulingInfrastructure_02.cpp
@@ -1,14 +1,36 @@
+/*
+Don't mind this attempt, it gets messy with member function pointers all over the place... 
+*/
+
+typedef void (*TickFunction)();
+
 class ApplicationScheduler
 {
-    void RegisterPlatformInitializeFunction(...);
-    void RegisterCoreInitializeFunction(...);
-    void RegisterSimulationInitializeFunction(...);
+public:
+    static void RegisterPlatformInitializeFunction(...);
+    static void RegisterPlatformTickFunction(TickFunction* tick);
+    static void RegisterPlatformTerminateFunction(...);
 
-    void RegisterPlatformTickFunction(...);
-    void RegisterCoreTickFunction(...);
-    void RegisterSimulationTickFunction(...);
+    static void RegisterCoreInitializeFunction(...);
+    static void RegisterCoreTickFunction(TickFunction* tick);
+    static void RegisterCoreTerminateFunction(...);
 
-    void RegisterPlatformTerminateFunction(...);
-    void RegisterCoreTerminateFunction(...);
-    void RegisterSimulationTerminateFunction(...);
+    static void RegisterSimulationInitializeFunction(...);
+    static void RegisterSimulationTickFunction(TickFunction* tick);
+    static void RegisterSimulationTerminateFunction(...);
+};
+
+class Renderer
+{
+public:
+    Renderer()
+    {
+        ApplicationScheduler::RegisterSimulationTickFunction(&Tick);
+    }
+
+private:
+    void Tick()
+    {
+
+    }
 };
