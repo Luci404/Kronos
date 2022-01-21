3bced3c7dc03accbc47e06181c028b97368b7e3e
Storm Søndergaard <stormsoendergaard@gmail.com>
2022-01-21 17:52:26 +0100

    RnD

--- RnD/KronosRnD/Scene01/main.cpp
+++ RnD/KronosRnD/Scene01/main.cpp
@@ -3,11 +3,15 @@
 #include <vector>
 #include <iostream>
 #include <concepts>
+#include <utility>
 
 class Entity
 {
 public:
-    Entity() { m_Name = "Entity"; }
+    Entity(const std::string& name) 
+        : m_Name(name)
+    { 
+    }
 
     virtual const std::string& GetName() const { return m_Name; }
 
@@ -15,10 +19,13 @@ protected:
     std::string m_Name;
 };
 
-class StaticMesh : public Entity 
+class StaticMesh : public Entity
 {
 public:
-    StaticMesh() { m_Name = "StaticMesh"; }
+    StaticMesh() 
+        : Entity("StaticMesh")
+    { 
+    }
 };
 
 class Scene
@@ -27,7 +34,7 @@ public:
     template<std::derived_from<Entity> T, typename... Args>
     T& CreateEntity(Args&&... args)
     {
-        return emplace_back(std::make_unique<T>(args));
+        return static_cast<T&>(*m_Entities.emplace_back(std::make_unique<T>(std::forward<Args>(args)...)));
     }
 
     void Print()
@@ -45,8 +52,8 @@ private:
 int main(int argc, char* argv[])
 {
     Scene scene;
-    
-    scene.CreateEntity<Entity>();
+
+    scene.CreateEntity<Entity>("Entity 01");
     scene.CreateEntity<StaticMesh>();
 
     scene.Print();
