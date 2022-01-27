c4798ceb4db5bbc9fcaf4e617810deb037fb499e
Storm Søndergaard <stormsoendergaard@gmail.com>
2022-01-27 01:52:35 +0100

    RnD

--- RnD/KronosRnD/DODArchitecture/DODArchitecture.cpp
+++ RnD/KronosRnD/DODArchitecture/DODArchitecture.cpp
@@ -10,82 +10,156 @@ RegisterIntegrant:
 #include <cassert>
 #include <concepts>
 
-// Integrant Core
 #define INTEGRANT_REGISTRY_SIZE 1024
 
-class IIntegrant {};
+#pragma region Integrant
+class IIntegrant
+{
+public:
+	virtual void Initialize() = 0;
+};
 
-class IIntegrantPool {};
+class TransformIntegrant final : public IIntegrant
+{
+public:
+	void Move(float x, float y, float z) {};
+};
+#pragma endregion 
+
+#pragma region IntegrantHandle
+class IIntegrantHandle
+{
+public:
+	friend class IntegrantRegistry;
+protected:
+	uint32_t m_Handle;
+};
+
 template<std::derived_from<IIntegrant> T>
-class IntegrantPool : public IIntegrantPool
+class IntegrantHandle final : public IIntegrantHandle
+{
+public:
+	T* operator->() const noexcept 
+	{
+		return *IntegrantRegistry::GetIntegrant<T>();
+	}
+};
+#pragma endregion
+
+#pragma region IntegrantPool
+class IIntegrantPool {};
+
+template<std::derived_from<IIntegrant> T>
+class IntegrantPool final : public IIntegrantPool
 {
 private:
     std::array<T, INTEGRANT_REGISTRY_SIZE> m_IntegrantArray;
 };
+#pragma endregion
 
+#pragma region IntegrantRegistry
 class IntegrantRegistry
 {
 public:
+	friend class IIntegrantHandle;
+
+public:
 	template<std::derived_from<IIntegrant> T>
-	void RegisterIntegrant() {}
+	static void RegisterIntegrant() {}
     
     template<std::derived_from<IIntegrant> T, typename... Args>
-    T* CreateIntegrant(Args&&... args) {}
+	static IntegrantHandle<T> CreateIntegrant(Args&&... args) {}
 
-	void DestroyIntegrant( /* ??? */) {}
+	static void DestroyIntegrant(IIntegrantHandle integrantHandle) {}
+
+	template<std::derived_from<IIntegrant> T>
+	static T& GetIntegrant() {};
+
+	template<std::derived_from<IIntegrant> T, typename F>
+	static void EachIntegrant(F& lambda) {};
 
 private:
-	std::unordered_map<const char*, std::shared_ptr<IntegrantPool>> m_IntegrantPools{};
+	IntegrantRegistry() {}
+
+private:
+	static std::unordered_map<const char*, std::shared_ptr<IIntegrantPool>> m_IntegrantPools;
 };
 
-// Integrant Types
-class TransformIntegrant : public IIntegrant {};
+std::unordered_map<const char*, std::shared_ptr<IIntegrantPool>> IntegrantRegistry::m_IntegrantPools = {};
+#pragma endregion
 
-class SomeModule : public StaticModule
+#pragma region EngineCore
+class IEntity
 {
-	void Initialize() override
-	{
-		IntegrantRegistry::RegisterIntegrant<TransformIntegrant>();
-	}
+public: 
+	virtual void Initialize() = 0;
+	virtual void Terminate() = 0;
 };
 
-// Entity Core
-class IEntity {};
-
-class MoveableEntity : public IEntity
+class ISystem 
 {
 public:
-	void Initialize() override
-	{
-		m_TransformIntegrant = IntegrantRegistry::CreateIntegrant<TransformIntegrant>(this /* ? */);
-		// Consider adding integrants with a member function on the Entity class, that way, cleanup could be automatic.
-	}
-
-private:
-	TransformIntegrant* m_TransformIntegrant;
+	virtual void Initialize() = 0;
+	virtual void Terminate() = 0;
 };
+
+class IStaticModule
+{
+public:
+	virtual void Initialize() = 0;
+	virtual void Terminate() = 0;
+};
+#pragma endregion
+
+#pragma region ExampleModule
 
 // Renderer Module
-class MeshRendererIntegrant : public IIntegrant {};
+class ExampleIntegrant : public IIntegrant 
+{
+public:
+	ExampleIntegrant(int integer)
+		: ExampleData(integer)
+	{
+	}
 
-class RendererModule : public StaticModule
+	void DoStuff(float integer)
+	{
+		ExampleData += integer;
+	}
+
+public:
+	int ExampleData;
+};
+
+class ExampleStaticModule : public IStaticModule
 {
 public:
 	void Initialize() override
 	{
-		IntegrantRegistry::RegisterIntegrant<MeshRendererIntegrant>();
+		IntegrantRegistry::RegisterIntegrant<ExampleIntegrant>();
 	}
 };
 
-class RendererSystem : public ISystem 
+class ExampleSystem : public ISystem
 {
 public:
-	void SomeFunction()
+	void DoStuff()
 	{
-		// Look at EnTT, also, idk how lambdas work....
-		IntegrantRegistry::Each<MeshRendererIntegrant>([](MeshRendererIntegrant& meshRendererIntegrant) {
-			/* Do logic */
-			}
-		);
+		IntegrantRegistry::EachIntegrant<ExampleIntegrant>([](ExampleIntegrant& meshRendererIntegrant) { /* Do logic */ });
 	}
 };
+
+class ExampleEntity : public IEntity
+{
+public:
+	void Initialize() override
+	{
+		m_ExampleIntegrant = IntegrantRegistry::CreateIntegrant<ExampleIntegrant>(1234);
+		m_ExampleIntegrant->DoStuff(1234);
+		m_ExampleIntegrant->ExampleData += 1234;
+	}
+
+private:
+	IntegrantHandle<ExampleIntegrant> m_ExampleIntegrant;
+};
+#pragma endregion
