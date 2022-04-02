17a35b17f2b2ad2297287449201dc17880f2c083
Storm Søndergaard <stormsoendergaard@gmail.com>
2022-04-02 22:53:31 +0200

    Work on task system

--- RnD/ExecutionGraph/main.cpp
+++ RnD/ExecutionGraph/main.cpp
@@ -1,6 +1,8 @@
 #include <array>
 #include <inttypes.h>
 #include <deque>
+#include <iostream>
+#include <vector>
 
 #define MAXIMUM_TASK_DEPENDENCIES 4
 
@@ -14,12 +16,12 @@ public:
 	
 struct ExecutionDependency
 {
-	ExecutionDependency(EDependencyType dependencyType, IExecutionNode* dependent, IExecutionNode* dependency)
+	ExecutionDependency(const EDependencyType dependencyType, const IExecutionNode* dependent, const IExecutionNode* dependency)
 		: DependencyType(dependencyType), Dependent(dependent), Dependency(dependency) {}
 
-	EDependencyType DependencyType;
-	IExecutionNode* Dependent;
-    IExecutionNode* Dependency;
+	const EDependencyType DependencyType;
+	const IExecutionNode* Dependent;
+    const IExecutionNode* Dependency;
 };
 
 class ExecutionTask : public IExecutionNode
@@ -38,54 +40,83 @@ private:
 class ExecutionGraph : public IExecutionNode
 {
 public:
+    ExecutionGraph() : m_DebugName("UNNAMED") {}
+    ExecutionGraph(const std::string& debugName) : m_DebugName(debugName) {}
+
 	template<EDependencyType DependencyType>
 	void AddDependency(const IExecutionNode* dependent, const IExecutionNode* dependency)
 	{
 		m_ExecutionDependencies.push_back(ExecutionDependency(DependencyType, dependent, dependency));
 	}
 
-	virtual void Execute() const override { };
+	virtual void Execute() const override
+    {
+        std::cout << m_DebugName << std::endl;
+
+        for (const IExecutionNode* executionNode : m_ExecutionNodes)
+        {
+            executionNode->Execute();
+        }
+    }
 	
-	ExecutionTask* AppendTask(void(*function)())
+	IExecutionNode* AppendTask(void(*function)())
     {
         ExecutionTask* executionNode = new ExecutionTask(function);
         m_ExecutionNodes.push_back(static_cast<IExecutionNode*>(executionNode));
         return executionNode;    
     }
 
-	void AppendGraph(ExecutionGraph* executionGraph)
+	IExecutionNode* AppendGraph(ExecutionGraph* executionGraph)
     {
         m_ExecutionNodes.push_back(static_cast<IExecutionNode*>(executionGraph));
+        return executionGraph;
     }
-	
-	void Execute() { /* ... */ }
 
 private:
 	std::deque<IExecutionNode*> m_ExecutionNodes;
 	std::deque<ExecutionDependency> m_ExecutionDependencies;
+
+    const std::string m_DebugName;
 };
+
+class ExecutionScheduler
+{
+public:
+    static void AppendGraph(ExecutionGraph* executionGraph)
+    {
+        m_ExecutionNodes.push_back(static_cast<IExecutionNode*>(executionGraph));
+    }
+
+private:
+    static std::vector<IExecutionNode*> m_ExecutionNodes;
+};
+
+std::vector<IExecutionNode*> ExecutionScheduler::m_ExecutionNodes = std::vector<IExecutionNode*>();
+
+
+/// EXAMPLE
 
 class RendererModule
 {
 public:
-    static void BuildSceneData() {}
-    static void BuildCommandBuffers() {}
-    static void RenderScene() {}
-    static void QueueNextFrame() {}
+    static void BuildSceneData() { std::cout << "RendererModule::BuildSceneData()" << std::endl; }
+    static void BuildCommandBuffers() { std::cout << "RendererModule::BuildCommandBuffers()" << std::endl; }
+    static void RenderScene() { std::cout << "RendererModule::RenderScene()" << std::endl; }
+    static void QueueNextFrame() {std::cout << "RendererModule::QueueNextFrame()" << std::endl; }
 
 	static void Initialize()
 	{
-		ExecutionGraph rendererLoopExecutionGraph;
-		ExecutionTask* buildSceneDataNode = rendererLoopExecutionGraph.AppendTask(&RendererModule::BuildSceneData);
-		ExecutionTask* buildCommandBuffersNode = rendererLoopExecutionGraph.AppendTask(&RendererModule::BuildCommandBuffers);
-		ExecutionTask* renderSceneNode = rendererLoopExecutionGraph.AppendTask(&RendererModule::RenderScene);
-		ExecutionTask* queueNextFrameNode = rendererLoopExecutionGraph.AppendTask(&RendererModule::QueueNextFrame);
+		ExecutionGraph rendererLoopExecutionGraph = ExecutionGraph("Renderer Loop");
+		IExecutionNode* buildSceneDataNode = rendererLoopExecutionGraph.AppendTask(&RendererModule::BuildSceneData);
+		IExecutionNode* buildCommandBuffersNode = rendererLoopExecutionGraph.AppendTask(&RendererModule::BuildCommandBuffers);
+		IExecutionNode* renderSceneNode = rendererLoopExecutionGraph.AppendTask(&RendererModule::RenderScene);
+		IExecutionNode* queueNextFrameNode = rendererLoopExecutionGraph.AppendTask(&RendererModule::QueueNextFrame);
 		rendererLoopExecutionGraph.AddDependency<EDependencyType::Precede>(buildCommandBuffersNode, buildSceneDataNode);
 		rendererLoopExecutionGraph.AddDependency<EDependencyType::Precede>(renderSceneNode, buildCommandBuffersNode);
 		rendererLoopExecutionGraph.AddDependency<EDependencyType::Precede>(queueNextFrameNode, renderSceneNode);
-		rendererLoopExecutionGraph.AddDependency<EDependencyType::Conditional>(queueNextFrameNode, &RendererModule::ShouldQueueNextFrame);
+		//rendererLoopExecutionGraph.AddDependency<EDependencyType::Conditional>(queueNextFrameNode, RendererModule::ShouldQueueNextFrame);
 		
-		ExecutionScheduler::AppendGraph(rendererLoopExecutionGraph);
+		ExecutionScheduler::AppendGraph(&rendererLoopExecutionGraph);
 	}
 	
 	static void Terminate() { /* ... */ }
@@ -101,21 +132,21 @@ public:
 
 int main(int argc, char** argv)
 {
-	ExecutionGraph initializationExecutionGraph;
-	ExecutionNode rendererModuleInitializationNode = initializationExecutionGraph.AppendTask(&RendererModule::Initialize);
-	ExecutionNode sceneModuleInitializationNode = initializationExecutionGraph.AppendTask(&SceneModule::Initialize);
+	ExecutionGraph initializationExecutionGraph = ExecutionGraph("Application Initialization");
+	IExecutionNode* rendererModuleInitializationNode = initializationExecutionGraph.AppendTask(&RendererModule::Initialize);
+	IExecutionNode* sceneModuleInitializationNode = initializationExecutionGraph.AppendTask(&SceneModule::Initialize);
 	initializationExecutionGraph.AddDependency<EDependencyType::Precede>(rendererModuleInitializationNode, sceneModuleInitializationNode);
 
-	ExecutionGraph terminationExecutionGraph;
-	ExecutionNode rendererModuleTerminationNode = terminationExecutionGraph.AppendTask(&RendererModule::Terminate);
-	ExecutionNode sceneModuleTerminationNode = terminationExecutionGraph.AppendTask(&SceneModule::Terminate);
+	ExecutionGraph terminationExecutionGraph = ExecutionGraph("Application Termination");
+	IExecutionNode* rendererModuleTerminationNode = terminationExecutionGraph.AppendTask(&RendererModule::Terminate);
+	IExecutionNode* sceneModuleTerminationNode = terminationExecutionGraph.AppendTask(&SceneModule::Terminate);
 	terminationExecutionGraph.AddDependency<EDependencyType::Succeeds>(rendererModuleTerminationNode, sceneModuleTerminationNode);
 	
-	ExecutionGraph applicationExecutionGraph;
-	ExecutionNode initializationNode = applicationExecutionGraph.AppendGraph(initializationExecutionGraph);
-	ExecutionNode terminationNode = applicationExecutionGraph.AppendGraph(terminationExecutionGraph);
-	applicationExecutionGraph.AddDependency<EDependencyType::Conditional>(terminationNode, &Core::ShouldClose);
+	ExecutionGraph applicationExecutionGraph = ExecutionGraph("Application Master");
+	IExecutionNode* initializationNode = applicationExecutionGraph.AppendGraph(&initializationExecutionGraph);
+	IExecutionNode* terminationNode = applicationExecutionGraph.AppendGraph(&terminationExecutionGraph);
+	//applicationExecutionGraph.AddDependency<EDependencyType::Conditional>(terminationNode, &Core::ShouldClose);
 	applicationExecutionGraph.AddDependency<EDependencyType::Precede>(terminationNode, initializationNode);
 	
-	ExecutionScheduler::AppendGraph(applicationExecutionGraph);
+	ExecutionScheduler::AppendGraph(&applicationExecutionGraph);
 }
